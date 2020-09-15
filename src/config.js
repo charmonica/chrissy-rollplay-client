3bb9edeb2d97df56b17763d9c3513389f4921cb5
Chrissy Harmon <clio.christine.harmon@gmail.com>
2020-09-15 15:31:40 -0400

    "cleaned up"

--- src/config.js
+++ src/config.js
@@ -1,6 +1,3 @@
 export default {
   API_ENDPOINT: "https://shrouded-scrubland-53128.herokuapp.com/"
-  // API_KEY: process.env.ROLLPLAY_API_KEY
 }
-
-// 'http://localhost:5000 https://server-link.com
